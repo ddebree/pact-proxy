819c0540ea46cf24652a29feb22e8639d2578f8f
Dean de Bree <ddebree@gmail.com>
2017-04-25 22:25:34 +0200

    Produce a basic pact config to the console

--- src/main/java/io/github/ddebree/pact/proxy/filters/PactRecorderFilter.java
+++ src/main/java/io/github/ddebree/pact/proxy/filters/PactRecorderFilter.java
@@ -2,11 +2,18 @@ package io.github.ddebree.pact.proxy.filters;
 
 import javax.servlet.http.HttpServletRequest;
 
+import au.com.dius.pact.consumer.ConsumerPactBuilder;
+import au.com.dius.pact.model.PactWriter;
+import au.com.dius.pact.model.RequestResponsePact;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.ZuulFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Random;
 
 public class PactRecorderFilter extends ZuulFilter {
 
@@ -32,7 +39,29 @@ public class PactRecorderFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
 
+        Random random = new Random();
+
         log.info("{} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+        /*
+        See https://groups.google.com/forum/m/#!topic/pact-support/48AirZ-a5-s
+         */
+        RequestResponsePact pact = ConsumerPactBuilder
+                .consumer("client")
+                .hasPactWith("server")
+                .uponReceiving("a request_" + Math.abs(random.nextLong()))
+                .path(request.getRequestURL().toString())
+                .method(request.getMethod())
+                //.body(req.getEntity(String.class))
+                .willRespondWith()
+                //.status(res.getStatus())
+                //.body(res.getResponse().getEntity().toString())
+                .toFragment().toPact();
+
+        StringWriter strOut = new StringWriter();
+        PactWriter.writePact(pact, new PrintWriter(strOut));
+
+        log.info("Pact file: {}", strOut);
 
         return null;
     }
